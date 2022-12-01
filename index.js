076fc3cd5f74ff58aab97ff460befb19c3e84043
Who <ehreh789@naver.com>
2022-12-01 20:28:40 +0900

    20221201

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+var name = "Nicolas";
+name = "Pedro";
+*/
+
+const name = "Nicolas";
+name - "Pedro";
+
+const person = {
+    name : "Nicolas" 
+};
+
+person.name = "Pedro";
+
+let nico = "las";
+nico = "lalalalala";
