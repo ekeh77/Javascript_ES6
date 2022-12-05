a873953c5945c531f31118a3d3755839ae60b3a6
Who <ehreh789@naver.com>
2022-12-05 19:34:57 +0900

    20221205

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
+/***********Dead Zone**************/
 /*
 console.log(myName);
 var myName = "nico";
 */
 
-console.log(myName);
-let myName = "nico"
+//console.log(myName);
+//let myName = "nico"
+
+/***********Block Scope**************/
+
+if(true) {
+    var hello1 = "hi";
+}
+
+console.log(hello1);
+
+
+if(true) {
+    const hello2 = "hi";
+}
+
+console.log(hello2);
+
+function a (){
+    var hello3 = "h1";
+}
+
+console.log(hello3);
+
+if(true) {
+    let hello4 = "hi";
+}
+    let hello4 = "hi";
+console.log(hello4)
