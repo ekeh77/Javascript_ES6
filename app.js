ea73739df5bd31999d85be6454d5eff508c55d8c
Who <ehreh789@naver.com>
2022-12-14 19:32:21 +0900

    20221214

--- app.js
+++ app.js
@@ -126,6 +126,7 @@ console.log(sayHi());
 */
 
 /***********Sexy Strings****** */
+/*
 const sayHi = (aName = "anon") => `hello ${aName} lovely to have you`;
 
 console.log(sayHi());
@@ -135,3 +136,33 @@ console.log(`hello how ar you ${1000 * 100}`);
 const add = (a, b) => a + b;
 
 console.log(`hello how ar you ${add(6, 6)}`);
+*/
+/***************HTML Fragments********** */
+
+const wrapper = document.querySelector(".wrapper");
+/*
+const addWelcome = () => {
+    const div = document.createElement("div");
+    const h1 = document.createElement("h1");
+    h1.innerText = "Hello";
+    h1.className = "sexyTitle";
+    div.append(h1);
+    wrapper.append(div);
+}
+*/
+const addWelcome = () => {
+    const div = `
+        <div class="hello">
+            <h1 class="title">Hello</h1>
+        </div>
+    `;
+    wrapper.append(div);
+}
+
+console.log(`
+
+    hello
+
+`);
+
+setTimeout(addWelcome, 5000)
