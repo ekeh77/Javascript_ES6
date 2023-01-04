e35eae696560d22003efe10c044ff8f8b8ba1181
Who <ehreh789@naver.com>
2023-01-04 19:52:58 +0900

    20230104

--- app.js
+++ app.js
@@ -359,6 +359,7 @@ const settings = {
 */
 
 /***********Swapping and Skipping*********** */
+/*
 let mon = "Sat";
 let sat = "Mon";
 
@@ -368,3 +369,25 @@ const days = ["mon", "tue", "wed", "thu", "fri"];
 
 const [,,,thu,fri] = days;
 console.log(thu, fri);
+*/
+
+/************ Introduction to Spread *************/
+const friends = [1, 2, 3, 4];
+const family = ["a", "b", "c"];
+console.log(friends);
+console.log(...friends);
+console.log([friends, family]);
+console.log([...friends, ...family]);
+
+const sexy = {
+    name : "nico",
+    age : 24
+};
+
+const hello = {
+    sexy : true,
+    hello : "hello"
+};
+
+console.log({sexy, hello});
+console.log({...sexy, ...hello});
