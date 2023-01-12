5a5d4085eece1011f12663251da7344a65e3270c
Who <ehreh789@naver.com>
2023-01-12 19:19:30 +0900

    20230112

--- app.js
+++ app.js
@@ -494,7 +494,19 @@ for (const letter of "hello"){
 */
 
 /*************Introduction to Async***** */
+/*
 const hello = fetch("http://google.com");
 
 console.log("something");
 console.log(hello);
+*/
+
+/************Creating Promises******** */
+
+const amISexy = new Promise((resolve, reject) => {
+    setTimeout(resolve, 3000, "Yes you are!")
+});
+
+console.log(amISexy);
+
+setInterval(console.log, 1000, amISexy);
