07ee38423e978e3dbff44b0669908dd7bd8083b2
Who <ehreh789@naver.com>
2023-01-26 20:54:22 +0900

    20230126

--- app.js
+++ app.js
@@ -590,6 +590,7 @@ const p1 = new Promise(resolve, reject => {
 .finally(() => console.log("Im done"));
 */
 /***************Real world Promises****** */
+/*
 fetch("https://yts.am/api/v2/list_movies.json")
     .then(response => {
         console.log(response);
@@ -597,3 +598,27 @@ fetch("https://yts.am/api/v2/list_movies.json")
     })
     .then(json => console.log(json))
     .catch(e => console.log(`❌ ${e}`));
+    */
+
+/*********Async Await************* */
+const getMoviesPromise = () => {
+    fetch("https://yts.am/api/v2/list_movies.json")
+    .then(response => {
+        console.log(response);
+        return response.json();
+    })
+    .then(json => console.log(json))
+    .catch(e => console.log(`❌ ${e}`));
+}
+
+const getMoviesAsync = async() => {
+    const response = await fetch("https://yts.am/api/v2/list_movies.json");
+    const json = await response.json();
+    console.log(json);
+}
+
+getMoviesAsync();
+
+async function getMovies2() {
+
+}
