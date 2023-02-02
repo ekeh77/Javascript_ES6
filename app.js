3822048e6e5f5c68a3bdec48bfc016d752a09c7c
Who <ehreh789@naver.com>
2023-02-02 20:23:45 +0900

    20230202

--- app.js
+++ app.js
@@ -815,25 +815,27 @@ class Counter {
     constructor({initialNumber = 0, counterId, plusId, minusId}) {
         this.count = initialNumber;
         this.counter = document.getElementById(counterId);
+        this.counter.innerText = initialNumber;
         this.plusBtn = document.getElementById(plusId);
         this.minusBtn = document.getElementById(minusId);
         this.addEventListeners();
     }
-    addEventListeners(){
+    addEventListeners = () => {
         this.plusBtn.addEventListener("click", this.increase);
         this.minusBtn.addEventListener("click", this.decrease);
     }
-    increase() {
+    increase = () => {
         this.count = this.count + 1;
         this.repaintCount();
     }
-    decrease() {
+    decrease = () => {
         this.count = this.count - 1;
         this.repaintCount();
     }
-    repaintCount(){
+    repaintCount = () => {
         this.counter.innerText = this.count
     }
 }
 
 new Counter({counterId:"count", plusId:"add",  minusId:"minus"});
+new Counter({counterId:"count2", plusId:"add2",  minusId:"minus2", initialNumber:666});
